095ff4a6637248e123704a469deeb4726cb755a3
David Koncsol <davidkoncsol@Davids-MacBook-Pro.local>
2019-09-16 10:29:25 -0700

    added superhero model add and delete. Also view pages

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 # Create your models here.
+
+
+
+class Superhero(models.Model):  # Note that parens are optional if not inheriting from another class
+    name = models.CharField(max_length=100)
+    power = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    age = models.IntegerField()
+
+    	
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'superhero_id': self.id})
