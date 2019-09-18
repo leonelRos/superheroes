26f5f3edfcd91cc4562af4c032a8a070bfc391a5
David Koncsol <davidkoncsol@Davids-MacBook-Pro.local>
2019-09-18 14:24:21 -0700

    fixed heroku issue

--- superhero/settings.py
+++ superhero/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-
+import django_heroku
 import os
 
 
@@ -126,5 +126,5 @@ LOGIN_REDIRECT_URL = '/superheroes/'
 # the logout shold be change to main page '/'
 LOGOUT_REDIRECT_URL = '/'
 
-import django_heroku
+
 django_heroku.settings(locals())
